91931e2c3957a401daa7f0247be0b7758ae4099a
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-06-17 21:00:31 +0400

    Initial commit

---
+++ src/main/java/com/github/a1k28/App.java
@@ -0,0 +1,9 @@
+package com.github.a1k28;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/github/a1k28/ai/AIService.java
@@ -0,0 +1,5 @@
+package com.github.a1k28.ai;
+
+public interface AIService {
+    String generate(String prompt, String language);
+}
