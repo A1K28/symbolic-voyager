4299c99a7a74b6648b17208b8018be56ef24c339
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-06-21 15:09:37 +0400

    minor updates

---
+++ src/main/java/com/github/a1k28/model/evolution/EvolutionProperties.java
@@ -0,0 +1,6 @@
+package com.github.a1k28.model.evolution;
+
+public enum EvolutionProperties {
+    REMOVE_SUCCEEDING_TESTS,
+    MUTATOR_LEVEL;
+}
--- src/main/java/com/github/a1k28/model/mutator/MutatorProperties.java
+++
@@ -1,5 +0,0 @@
-package com.github.a1k28.model.mutator;
-
-public enum MutatorProperties {
-    REMOVE_SUCCEEDING_TESTS;
-}
