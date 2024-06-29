b7a78560808fd1672bd0de6c146c0d9fedefee66
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-06-29 20:32:31 +0400

    delete claude

--- src/main/java/com/github/a1k28/evoc/core/ai/AIService.java
+++
@@ -1,5 +0,0 @@
-package com.github.a1k28.evoc.core.ai;
-
-public interface AIService {
-    String generateTestClass(String prompt, boolean retry);
-}
--- src/main/java/com/github/a1k28/evoc/model/ai/claude/input/ClaudeMessage.java
+++
@@ -1,7 +0,0 @@
-package com.github.a1k28.evoc.model.ai.claude.input;
-
-import com.google.gson.JsonObject;
-
-public interface ClaudeMessage {
-    JsonObject toJsonObject();
-}
