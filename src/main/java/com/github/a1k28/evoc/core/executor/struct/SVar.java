ecfcd17e17491c3f013f16545cf24186e7e7b6f9
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-07-07 13:21:36 +0400

    minor upd

--- src/main/java/com/github/a1k28/evoc/core/executor/struct/SVar.java
+++ src/main/java/com/github/a1k28/evoc/core/executor/struct/SVar.java
@@ -2,15 +2,13 @@ package com.github.a1k28.evoc.core.executor.struct;
 
 import com.microsoft.z3.Expr;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.Setter;
 import sootup.core.jimple.basic.Value;
 
-@Getter
 @Setter
 @AllArgsConstructor
 public class SVar {
-    private String name;
+    private final String name;
     private final Value value;
     private final Expr exrp;
     private final boolean isOriginal;
