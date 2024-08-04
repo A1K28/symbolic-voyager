353978120ad6553adbb9590c3d0a8c371cae95f0
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-08-04 15:01:15 +0400

    add mkSentinel method to z3 helper

--- src/main/java/com/github/a1k28/evoc/core/z3extended/Z3Helper.java
+++ src/main/java/com/github/a1k28/evoc/core/z3extended/Z3Helper.java
@@ -10,18 +10,28 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Z3Helper {
+    private static final Map<Sort, Expr> sentinels = new HashMap<>();
     private static final Map<Sort, TupleSort> sortMap = new HashMap<>();
 
     public static TupleSort mapSort(Context ctx, Sort sort) {
         if (!sortMap.containsKey(sort)) {
             TupleSort tupleSort = ctx.mkTupleSort(
-                    ctx.mkSymbol("nullable:"+sort),
-                    new Symbol[]{ctx.mkSymbol("value"), ctx.mkSymbol("isNull")},
+                    ctx.mkSymbol("optional:"+sort),
+                    new Symbol[]{ctx.mkSymbol("value"), ctx.mkSymbol("isEmpty")},
                     new Sort[]{sort, ctx.getBoolSort()}
             );
             sortMap.put(sort, tupleSort);
         }
         return sortMap.get(sort);
+    }
+
+    public static Expr mkSentinel(Context ctx, Sort sort) {
+        if (!sentinels.containsKey(sort)) {
+            Expr sentinel = mapSort(ctx, sort).mkDecl().apply(
+                    ctx.mkConst("sentinel", sort), ctx.mkTrue());
+            sentinels.put(sort, sentinel);
+        }
+        return sentinels.get(sort);
     }
 
     public static Sort getSort(Context ctx, Expr expr) {
