50cf95e6952f55a96a6d819660c4e04d4d519565
Khorbaladze A <aleksandrekhorbaladze@gmail.com>
2024-09-27 11:36:52 +0400

    upd model

--- src/main/java/com/github/a1k28/evoc/core/symbolicexecutor/model/HandlerNode.java
+++ src/main/java/com/github/a1k28/evoc/core/symbolicexecutor/model/HandlerNode.java
@@ -2,12 +2,10 @@ package com.github.a1k28.evoc.core.symbolicexecutor.model;
 
 import com.github.a1k28.evoc.core.symbolicexecutor.struct.SCatchNode;
 import com.github.a1k28.evoc.core.symbolicexecutor.struct.SMethodPath;
-import com.github.a1k28.evoc.core.symbolicexecutor.struct.SNode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import sootup.core.jimple.basic.Trap;
 
 @Getter
 @Setter
